3a7faca41154f42f94c3783e14e9d83ed4f4f526
SREEHARI K.V <sreeharivijayan619@gmail.com>
2017-11-12 21:36:48 +0530

    added treasure-images

--- main_app/views.py
+++ main_app/views.py
@@ -7,15 +7,17 @@ def index(request):
 
 
 class Treasure:
-    def __init__(self, name, value, material, location):
+    def __init__(self, name, value, material, location, image_url):
         self.name = name
         self.value = value
         self.material = material
         self.location = location
+        self.image_url = image_url
 
 
 treasures = [
-    Treasure('Gold nuggets', 100.00, 'gold', 'silicon valley'),
-    Treasure('silver nuggets', 50.00, 'silver', 'london'),
-    Treasure('coffee gold', 0.00, 'gold', 'silicon valley')
+    Treasure(' Coffee can', 100.00, ' Brass', ' silicon valley', 'http://139.59.68.161/media/treasure_images/Coffee%20Can/treasuregram-brand-coffee-can.png'),
+    Treasure(' Gold nuggets', 50.00, ' Gold', 'london', 'http://139.59.68.161/media/treasure_images/Gold%20nugget/treasuregram-brand-gold-nugget.png'),
+    Treasure(' Arrow head', 0.00, ' Iron', ' Germany', 'http://139.59.68.161/media/treasure_images/Arrowhead/treasuregram-brand-arrowhead.png'),
+    Treasure(' Fools gold', 120.00, 'German silver', 'USA', 'http://139.59.68.161/media/treasure_images/Gold%20nugget/monkey_sitting_tall.png')
 ]
