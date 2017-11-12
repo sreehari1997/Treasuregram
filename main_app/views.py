ee778589201026b1c9f83f0b4194cdeb3909df86
SREEHARI K.V <sreeharivijayan619@gmail.com>
2017-11-12 22:58:26 +0530

    looks good

--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render
+from .models import Treasure
 # Create your views here.
 
 
 def index(request):
+    treasures = Treasure.objects.all()
     return render(request, 'index.html', {'treasures': treasures})
 
 
-class Treasure:
-    def __init__(self, name, value, material, location, image_url):
-        self.name = name
-        self.value = value
-        self.material = material
-        self.location = location
-        self.image_url = image_url
-
-
-treasures = [
-    Treasure(' Coffee can', 100.00, ' Brass', ' silicon valley', 'http://139.59.68.161/media/treasure_images/Coffee%20Can/treasuregram-brand-coffee-can.png'),
-    Treasure(' Gold nuggets', 50.00, ' Gold', 'london', 'http://139.59.68.161/media/treasure_images/Gold%20nugget/treasuregram-brand-gold-nugget.png'),
-    Treasure(' Arrow head', 0.00, ' Iron', ' Germany', 'http://139.59.68.161/media/treasure_images/Arrowhead/treasuregram-brand-arrowhead.png'),
-    Treasure(' Fools gold', 120.00, 'German silver', 'USA', 'http://139.59.68.161/media/treasure_images/Gold%20nugget/monkey_sitting_tall.png')
-]
+# class Treasure:
+#     def __init__(self, name, value, material, location, image_url):
+#         self.name = name
+#         self.value = value
+#         self.material = material
+#         self.location = location
+#         self.image_url = image_url
+#
+#
+# treasures = [
+#     Treasure(' Coffee can', 100.00, ' Brass', ' silicon valley', 'http://139.59.68.161/media/treasure_images/Coffee%20Can/treasuregram-brand-coffee-can.png'),
+#     Treasure(' Gold nuggets', 50.00, ' Gold', 'london', 'http://139.59.68.161/media/treasure_images/Gold%20nugget/treasuregram-brand-gold-nugget.png'),
+#     Treasure(' Arrow head', 0.00, ' Iron', ' Germany', 'http://139.59.68.161/media/treasure_images/Arrowhead/treasuregram-brand-arrowhead.png'),
+#     Treasure(' Fools gold', 120.00, 'German silver', 'USA', 'http://139.59.68.161/media/treasure_images/Gold%20nugget/monkey_sitting_tall.png')
+# ]
