9dee522fbe395eacbfa3b257850034c8164ce5dc
SREEHARI K.V <sreeharivijayan619@gmail.com>
2017-11-12 10:49:27 +0530

    passed list objects to view

--- main_app/views.py
+++ main_app/views.py
@@ -3,8 +3,19 @@ from django.shortcuts import render
 
 
 def index(request):
-    name = 'gold_nuggets'
-    value = 1000
-    context = {'treasure_name': name,
-               'treasure_value': value}
-    return render(request, 'index.html', context)
+    return render(request, 'index.html', {'treasures': treasures})
+
+
+class Treasure:
+    def __init__(self, name, value, material, location):
+        self.name = name
+        self.value = value
+        self.material = material
+        self.location = location
+
+
+treasures = [
+    Treasure('Gold nuggets', 100, 'gold', 'silicon valley'),
+    Treasure('silver nuggets', 50, 'silver', 'london'),
+    Treasure('coffee gold', 100, 'gold', 'silicon valley')
+]
